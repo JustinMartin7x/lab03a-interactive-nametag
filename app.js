578f66accbf171269b0ed4cd71c022ba3d46e48e
JustinMartin7x <justin.martin7x@gmail.com>
2020-09-23 16:23:53 -0700

    adding more color buttons

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const main = document.getElementById ('main')
 const blueButton = document.getElementById ('blueButton')
 const blackButton = document.getElementById ('blackButton')
 const grayButton = document.getElementById ('grayButton')
+const pinkButton = document.getElementById ('pinkButton')
+const lightGreenButton = document.getElementById ('lightGreenButton') 
+const lightBlueButton = document.getElementById ('lightBlueButton')
 
 blueButton.addEventListener( 'click', () => {
     
@@ -29,4 +32,18 @@ grayButton.addEventListener('click', () => {
 
     main.style.background = 'gray'
 
+})
+pinkButton.addEventListener('click' () => {
+
+    main.style.background = 'pink'
+
+})
+lightGreenButton.addEventListener('click', () => {
+
+    main.style.background = 'lightgreen'
+
+})
+lightBlueButton.addEventListener('click', () => {
+
+    main.style.background = 'lightblue'
 })
